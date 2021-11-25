14240280b122814ec8fd02d4d8102e93838dd5cf
Your Name <you@example.com>
2021-11-25 20:41:20 -0300

    update

--- userPage/script.js
+++ userPage/script.js
@@ -2,24 +2,21 @@ let user = document.location.search;
 user = user.replace('?', '');
 
 getUserGithub(user);
-
+getUserReposGithub(user);
 
 function getUserGithub(userName) {
     fetch('https://fake-github.herokuapp.com/api/search/' + userName)
         .then(function (resultado) {
             resultado.json().then(function (data) {
                 console.log('User Data:', data);
-                showAvatarGithub(data);
                 showNameGithub(data);
                 showLoginGithub(data);
+                showAvatarGithub(data);
             });
         }).catch(function (erro) {
             console.log('erro:', erro);
         });
 }
-
-
-getUserReposGithub(user);
 
 
 function getUserReposGithub(userName) {
@@ -44,25 +41,30 @@ function showAvatarGithub(userName) {
 
 function showNameGithub(name){
     let div = document.createElement('div');
-    div.innerText = name.name;
+    div.id = '#name'
+    div.innerText = 'Name: ' + name.name;
     document.body.appendChild(div);
 }
 
 function showLoginGithub(login){
     let div = document.createElement('div');
-    div.innerText = login.login;
+    div.id = '#login'
+    div.innerText = 'Login: ' + login.login;
     document.body.appendChild(div);
 }
 
 function showNamerepos(repos){
     let div = document.createElement('div');
-    div.innerText = repos.name;
+    div.id = '#repos'
+    div.innerText = 'Repositorios: ' + repos.name;
     document.body.appendChild(div);
 }
 
 function showLinkrepos(a){
     let div = document.createElement('a')
+    div.id = '#link'
     div.href = a.svn_url
-    div.innerText = a.svn_url;
+    div.innerText = 'Links: '+ a.svn_url;
+    div.style.color = 'black';
     document.body.appendChild(div);
 }
