b5adbc60424dd3187db7a2e7fe82e14c9b41483a
Your Name <you@example.com>
2021-11-24 21:28:43 -0300

    update

--- main.js
+++ main.js
@@ -1,5 +1,72 @@
+const listUser = [
+    { name: 'Diego Planinscheck', userName: 'frst157'},
+    { name: 'Bruno Henrique Verbinnen de Carvalho', userName: 'brunohvc' },
+    { name: 'Vytor Augusto Rosa', userName: 'K43RU' },
+    { name: 'João Henrique Meireles da Silva', userName: 'nihilth' },
+    { name: 'Otavio Augusto dos Santos', userName: 'SantOtavio' },
+    { name: 'Camilly de Souza Pessotti', userName: 'camillyPessotti' },
+    { name: 'Thiago Marins Braga', userName: 'ThiagoBrag' },
+    { name: 'Ester Girelli', userName: 'Esterzinha12' },
+    { name: 'Gustavo Rebelatto Zapella', userName: 'rebelattogustavo' },
+    { name: 'Henrique Cole Fernandes', userName: 'HenriqueCole' },
+    { name: 'Kenzo Hideaky Ferreira Sato', userName: 'Kenzohfs' },
+    { name: 'Vinícius Bonatti Benner', userName: 'viniciusz1' },
+    { name: 'Leonardo Heitor Poglia', userName: 'leopoglia' },
+    { name: 'Felipe Mielke Vieira', userName: 'FelipeMielkeVieira' },
+    { name: 'Eduarda Bolgenhagen De Campos', userName: 'eduardabolgenhagen' },
+    { name: 'Matheus Franzener Hohmann', userName: 'MatheusFranzener' },
+    { name: 'Leonardo Giuseppe de Souza Rafaelli', userName: 'LeonardoRafaelli' },
+    { name: 'Camilly Vitoria da Rocha Goltz', userName: 'VitoriaCamilly' },
+    { name: 'Bruna Alves Mafra', userName: 'BMafra' },
+    { name: 'Otavio Matheus Neves', userName: 'otavionvs' }
+];
+
+function table() {
+    const table = document.createElement('table');
+    const row = document.createElement('tr');
+    const columnName = document.createElement('th');
+    const columnUserName = document.createElement('th');
+
+    columnName.innerText = 'Nome';
+    columnUserName.innerText = 'UserName';
+
+    row.appendChild(columnName);
+    row.appendChild(columnUserName);
+    table.appendChild(row);
+
+    listUser.forEach(function (element) {
+        const linha = pegarUsuario(
+            element.name,
+            element.userName);
+
+        table.appendChild(linha);
+    })
+    document.body.appendChild(table);
+}
+table();
+
+function pegarUsuario(name, userName) {
+    const row = document.createElement('tr');
+    const columnName = document.createElement('td');
+    const columnUserName = document.createElement('td');
+    const buttonUsuario = document.createElement('button');
+    const a = document.createElement('a');
+
+    columnName.innerText = name;
+    columnUserName.innerText = userName;
+    a.innerText = 'Verificar'
+    a.href='./userPage/index.html?' + userName;
+
+    row.appendChild(columnName);
+    row.appendChild(columnUserName);
+    row.appendChild(buttonUsuario);
+    buttonUsuario.appendChild(a);
+    return row;
+}
+
+
 function getUserGithub(userName) {
-    fetch('https://api.github.com/users/' + userName)
+    fetch('https://fake-github.herokuapp.com/api/' + userName)
         .then(function (resultado) {
             resultado.json().then(function (data) {
                 console.log('User Data:', data);
@@ -27,12 +94,3 @@ function getUserReposGithub(userName) {
             console.log('erro:', erro);
         });
 }
-
-
-// const listUser = [
-//     { name: 'Bruno Henrique', userName: 'brunohvc' },
-//     { name: 'Bruno Henrique', userName: 'brunohvc' },
-//     { name: 'Bruno Henrique', userName: 'brunohvc' },
-//     { name: 'Bruno Henrique', userName: 'brunohvc' },
-//     { name: 'Bruno Henrique', userName: 'brunohvc' },
-// ]
